5376c560c2216ceb8b8b7eb4022aac79c676b213
Reese Sorrell <joreesesorrell@gmail.com>
2023-05-07 17:57:34 -0500

    Fix padding on player cards

--- src/pages/components/playercard.js
+++ src/pages/components/playercard.js
@@ -18,7 +18,7 @@ const PlayerCard = ({ summonerName, championImages, laneImage, inverted, onExpan
     <div class={`container flex flex-nowrap 2xl:w-3/4 xl:w-2/4 md:w-2/4 sm: w-2/4 items-center mx-auto mt-10 hover:scale-100 transition-all duration-300 cursor-pointer ${
       selected ? '-translate-x-4' : ''
     }`} onClick={() => handleExpand(data)}>
-      <div className={`grid grid-rows-2 gap-y-0 ${
+      <div className={`grid p-1.5 grid-rows-2 gap-y-0 ${
           inverted ? 'bg-white' : 'bg-red-900'  
         } bg-opacity-100 rounded-md shadow-2xl w-full`}style={{ gridTemplateRows: "auto minmax(0, 1fr)" }}>
         <div class="container flex justify-between ml-1 h-8">
